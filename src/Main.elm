41d03ad09a8681981545d8a455575a77268d08aa
Julio César <zzantares@gmail.com>
2017-06-30 17:55:23 -0500

    Add basic styling using elm-mdl : Implements Material Design

--- src/Main.elm
+++ src/Main.elm
@@ -4,9 +4,17 @@ import Auth
 import Contentful
 import Debug exposing (log)
 import Html exposing (..)
+import Html.Attributes exposing (style)
 import Http
 import Json.Decode as Json exposing (..)
 import Json.Decode.Pipeline exposing (decode, required)
+import Material
+import Material.Color as Color
+import Material.Elevation as Elevation
+import Material.Grid exposing (..)
+import Material.Layout as Layout
+import Material.Options as Options exposing (css)
+import Material.Scheme
 
 
 main =
@@ -21,12 +29,13 @@ main =
 type alias Model =
     { contents : String
     , entries : List Contentful.Entry
+    , mdl : Material.Model
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "" []
+    ( Model "" [] Material.model
     , getContentful
     )
 
@@ -34,6 +43,7 @@ init =
 type Msg
     = ApiResponded (Result Http.Error String)
     | HandleEntriesResponse (Result Http.Error (List Contentful.Entry))
+    | Mdl (Material.Msg Msg)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -61,9 +71,33 @@ update msg model =
             in
                 ( model, Cmd.none )
 
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
 
 view : Model -> Html Msg
 view model =
+    Material.Scheme.topWithScheme Color.Blue Color.DeepOrange <|
+        Layout.render Mdl
+            model.mdl
+            [ Layout.fixedHeader
+            ]
+            { header =
+                [ h2 [ style [ ( "padding", "2rem" ) ] ]
+                    [ text "Elm, Contentful & Fun with Decoders" ]
+                ]
+            , drawer = []
+            , tabs = ( [], [] )
+            , main =
+                [ grid []
+                    [ cell [ size All 6, offset All 3 ] [ viewBody model ]
+                    ]
+                ]
+            }
+
+
+viewBody : Model -> Html Msg
+viewBody model =
     div []
         [ span [] [ text model.contents ]
         , br [] []
@@ -73,8 +107,12 @@ view model =
 
 viewEntry : Contentful.Entry -> Html Msg
 viewEntry entry =
-    div []
-        [ h2 [] [ text entry.title ]
+    Options.div
+        [ Elevation.e6
+        , css "margin-bottom" "20px"
+        , css "padding" "20px"
+        ]
+        [ h3 [] [ text entry.title ]
         , article [] [ text entry.body ]
         ]
 
